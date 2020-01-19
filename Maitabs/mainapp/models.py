d596ba2e0e7bf6618615b16496fb2c956061e306
Jofre <itiolpz@gmail.com>
2020-01-19 16:25:45 -0300

    Adicionando tabelas genero/artista/album

--- Maitabs/mainapp/models.py
+++ Maitabs/mainapp/models.py
@@ -4,3 +4,14 @@ class Usuario(models.Model):
     id = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=70)
 
+class Artista(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=70)
+
+class Album(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=70)
+
+class Genero(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=70)
