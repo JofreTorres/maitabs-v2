f258e3b0a626de0c32b050119850999f43d653bc
Felipe Torres <felipetorres@pop-os.localdomain>
2020-01-20 21:52:14 -0200

    remove ids dos modelos

--- Maitabs/mainapp/models.py
+++ Maitabs/mainapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Musica(models.Model):
-    id = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=70)
     artista = models.ForeignKey('Artista', on_delete=models.CASCADE)
     genero = models.ForeignKey('Genero', on_delete=models.CASCADE)
@@ -11,35 +10,30 @@ class Musica(models.Model):
         return self.nome
 
 class Usuario(models.Model):
-    id = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=70)
 
     def __str__(self):
         return "%s %s" % (self.id, self.nome)
 
 class Artista(models.Model):
-    id = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=70)
 
     def __str__(self):
         return "%s %s" % (self.id, self.nome)
 
 class Album(models.Model):
-    id = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=70)
 
     def __str__(self):
         return "%s %s" % (self.id, self.nome)
 
 class Genero(models.Model):
-    id = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=70)
 
     def __str__(self):
         return "%s %s" % (self.id, self.nome)
 
 class Playlist(models.Model):
-    id = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=70)
     criador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
