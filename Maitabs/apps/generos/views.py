baabba355ca6ea19f84fc83e35e2c16a6ee99712
Felipe Torres <felipetorres@pop-os.localdomain>
2020-01-25 15:51:30 -0200

    crud de generos

--- Maitabs/apps/generos/views.py
+++ Maitabs/apps/generos/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
 from .forms import GeneroForm
+from .models import Genero
 
 def index(request):
     template = loader.get_template('generos/index.html')
-    context = {}
+
+    generos = Genero.objects.all()
+
+    context = {
+        'generos': generos
+    }
+
     return HttpResponse(template.render(context, request))
 
 
@@ -30,3 +37,38 @@ def criar(request):
     }
 
     return render(request, context)
+
+
+def editar(request, id):
+    template = loader.get_template('generos/editar.html')
+
+    genero = Genero.objects.get(pk=id)
+
+    form = GeneroForm(instance=genero)
+
+    context = {
+        'form': form,
+        'genero': genero
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def atualizar(request, id):
+    genero = Genero.objects.get(pk=id)
+    form = GeneroForm(request.POST or None, instance=genero)
+
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, context)
+
+
+def deletar(request, id):
+    genero = Genero.objects.get(pk=id)
+    genero.delete()
+    return redirect('index')
