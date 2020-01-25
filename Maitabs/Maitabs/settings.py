3eccc4d30efd86964adaa64d98aea4b685b45848
Felipe Torres <felipetorres@pop-os.localdomain>
2020-01-25 14:26:57 -0200

    reestruturando arquivo apps

--- Maitabs/Maitabs/settings.py
+++ Maitabs/Maitabs/settings.py
@@ -31,13 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'mainapp',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # APPS CUSTOMIZÁVEIS
+    'apps.mainapp',
+    'apps.genero',
 ]
 
 MIDDLEWARE = [
--- Maitabs/Maitabs/urls.py
+++ Maitabs/Maitabs/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls'))
+    path('', include('apps.mainapp.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
