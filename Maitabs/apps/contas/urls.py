e20fae9a40e8de7874884d9fbd51f06f6dfa006d
Felipe Torres <felipetorres@pop-os.localdomain>
2020-02-29 18:46:58 -0300

    autorizacao e autenticacao

--- Maitabs/apps/contas/urls.py
+++ Maitabs/apps/contas/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='contas_index'),
+    path('nova', views.nova, name='contas_nova'),
+    path('mudar_senha', views.mudar_senha, name='contas_mudar_senha'),
+    path('login', views.login, name='contas_login'),
+    path('logout', views.logout, name='contas_logout'),
 ]
